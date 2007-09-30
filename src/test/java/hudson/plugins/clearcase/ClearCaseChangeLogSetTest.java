ab624d745a213f8508c516096eaecdc98506b5d2
redsolo <redsolo@dev.java.net>
2007-09-30 08:37:58 +0000

    Fixed the "Recent changes" and "Changes" pages so they show the annotation message instead of the raw message, and a NPE as the parent of the log entry wasnt set properly.

--- src/test/java/hudson/plugins/clearcase/ClearCaseChangeLogSetTest.java
+++ src/test/java/hudson/plugins/clearcase/ClearCaseChangeLogSetTest.java
@@ -16,4 +16,12 @@ public class ClearCaseChangeLogSetTest {
 		
 		Assert.assertEquals("Number of logs are incorrect", 3, logs.size());
 	}
+	
+	@Test
+	public void testGetParent() throws IOException, SAXException {
+		ClearCaseChangeLogSet logSet = ClearCaseChangeLogSet.parse(null, getClass().getResourceAsStream("changelog.xml"));
+		List<ClearCaseChangeLogEntry> logs = logSet.getLogs();
+		
+		Assert.assertNotNull("The parent of the first entry is null", logs.get(0).getParent());
+	}
 }
