e4b94417dc1eef480884c2de655aae585b76030d
redsolo <redsolo@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-10-01 19:32:40 +0000

    Fixed so the log is stored in unicode
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/plugins@5057 71c3de6d-444a-0410-be80-ed276b4c234a

--- src/test/java/hudson/plugins/clearcase/ClearCaseChangeLogSetTest.java
+++ src/test/java/hudson/plugins/clearcase/ClearCaseChangeLogSetTest.java
@@ -1,6 +1,10 @@
 package hudson.plugins.clearcase;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Assert;
@@ -23,5 +27,26 @@ public class ClearCaseChangeLogSetTest {
 		List<ClearCaseChangeLogEntry> logs = logSet.getLogs();
 		
 		Assert.assertNotNull("The parent of the first entry is null", logs.get(0).getParent());
+	}
+	
+	@Test
+	public void testUnicodeXml() throws IOException, SAXException {
+		Object[] logEntry = new Object[6];
+		logEntry[2] = "Bülow";
+		
+		List<Object[]> history = new ArrayList<Object[]>();
+		history.add(logEntry);
+		
+		File tempLogFile = File.createTempFile("clearcase", "xml");
+		FileOutputStream fileOutputStream = new FileOutputStream(tempLogFile);
+		
+		ClearCaseChangeLogSet.saveToChangeLog(fileOutputStream, history);
+		fileOutputStream.close();
+		
+		FileInputStream fileInputStream = new FileInputStream(tempLogFile);
+		ClearCaseChangeLogSet logSet = ClearCaseChangeLogSet.parse(null, fileInputStream);
+		List<ClearCaseChangeLogEntry> logs = logSet.getLogs();
+		
+		Assert.assertEquals("The comment wasnt correct", "Bülow", logs.get(0).getComment());
 	}
 }
