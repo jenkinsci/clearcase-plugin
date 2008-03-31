e1a1d6cfa76621c171c84f97474be52e425ea4a7
redsolo <redsolo@dev.java.net>
2008-03-31 06:49:26 +0000

    [clearcase-plugin] Fixed end line formatting on source file

--- src/main/java/hudson/plugins/clearcase/ClearCaseChangeLogParser.java
+++ src/main/java/hudson/plugins/clearcase/ClearCaseChangeLogParser.java
@@ -1,21 +1,16 @@
 package hudson.plugins.clearcase;
-
 import java.io.File;
 import java.io.IOException;
-
 import org.xml.sax.SAXException;
-
 import hudson.model.AbstractBuild;
 import hudson.scm.ChangeLogParser;
 import hudson.scm.ChangeLogSet;
-
 /***********************************************************************************************************************
  * ClearCase change log parser.
  * 
  * @author Erik Ramfelt
  */
 public class ClearCaseChangeLogParser extends ChangeLogParser {
-
     @Override
     public ChangeLogSet<ClearCaseChangeLogEntry> parse(AbstractBuild build, File changelogFile) throws IOException,
             SAXException {
