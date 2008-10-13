361f893e63aaf5315a67c327d640a5f7405c666c
sunfire <sunfire@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-10-13 20:28:12 +0000

    [FIXED HUDSON-1600]
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/plugins@12590 71c3de6d-444a-0410-be80-ed276b4c234a

--- src/main/java/hudson/plugins/clearcase/action/UcmSnapshotCheckoutAction.java
+++ src/main/java/hudson/plugins/clearcase/action/UcmSnapshotCheckoutAction.java
@@ -2,41 +2,72 @@ package hudson.plugins.clearcase.action;
 
 import hudson.FilePath;
 import hudson.Launcher;
-import hudson.Util;
 import hudson.plugins.clearcase.ClearTool;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
- * Check out action that will check out files into a UCM snapshot view.
- * Checking out the files will also udpate the load rules in the view.
+ * Check out action that will check out files into a UCM snapshot view. Checking
+ * out the files will also update the load rules in the view.
  */
 public class UcmSnapshotCheckoutAction implements CheckOutAction {
 
-    private ClearTool cleartool;
-    private String stream;
-    private String loadRules;
-        
-    public UcmSnapshotCheckoutAction(ClearTool cleartool, String stream, String loadRules) {
-        super();
-        this.cleartool = cleartool;
-        this.stream = stream;
-        this.loadRules = loadRules;
-    }
+	private ClearTool cleartool;
+	private String stream;
+	private String loadRules;
 
-    public boolean checkout(Launcher launcher, FilePath workspace, String viewName) throws IOException, InterruptedException {        
-        boolean localViewPathExists = new FilePath(workspace, viewName).exists();
-        
-        if (localViewPathExists) {
-            cleartool.rmview(viewName);
-        }
+	public UcmSnapshotCheckoutAction(ClearTool cleartool, String stream,
+			String loadRules) {
+		super();
+		this.cleartool = cleartool;
+		this.stream = stream;
+		this.loadRules = loadRules;
+	}
 
-        cleartool.mkview(viewName, stream);
+	public boolean checkout(Launcher launcher, FilePath workspace,
+			String viewName) throws IOException, InterruptedException {
+		boolean localViewPathExists = new FilePath(workspace, viewName)
+				.exists();
 
-        for (String loadRule : loadRules.split("\n")) {
-            cleartool.update(viewName, loadRule.trim());
-        }
-        return true;
-    }
+		if (localViewPathExists) {
+			String configSpec = cleartool.catcs(viewName);
+			Set<String> configSpecLoadRules = extractLoadRules(configSpec);
+			boolean recreate = currentConfigSpecUptodate(configSpecLoadRules);
+			if (recreate) {
+				cleartool.rmview(viewName);
+				cleartool.mkview(viewName, stream);
+			}
+		} else {
+			cleartool.mkview(viewName, stream);
+		}
+
+		for (String loadRule : loadRules.split("\n")) {
+			cleartool.update(viewName, loadRule.trim());
+		}
+		return true;
+	}
+
+	private boolean currentConfigSpecUptodate(Set<String> configSpecLoadRules) {
+		boolean recreate = false;
+		for (String loadRule : loadRules.split("\n")) {
+			if (!configSpecLoadRules.contains(loadRule)) {
+				recreate  = true;
+			}
+		}
+		return recreate;
+	}
+
+	private Set<String> extractLoadRules(String configSpec) {
+		Set<String> rules = new HashSet<String>();
+		for (String row : configSpec.split("\n")) {
+			String trimmedRow = row.toLowerCase().trim();
+			if (trimmedRow.startsWith("load")) {
+				rules.add(row.trim().substring("load".length()).trim());
+			}
+		}
+		return rules;
+	}
 
 }
