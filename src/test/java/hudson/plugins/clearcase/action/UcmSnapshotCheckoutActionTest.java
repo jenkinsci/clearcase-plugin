97867c84d62eb03ee81394e1e44f1f222d3d0c90
sunfire <sunfire@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-10-14 07:57:04 +0000

    [FIXED HUDSON-1600]
    git-svn-id: https://svn.dev.java.net/svn/hudson/trunk/hudson/plugins/clearcase@12592 71c3de6d-444a-0410-be80-ed276b4c234a

--- src/test/java/hudson/plugins/clearcase/action/UcmSnapshotCheckoutActionTest.java
+++ src/test/java/hudson/plugins/clearcase/action/UcmSnapshotCheckoutActionTest.java
@@ -1,7 +1,5 @@
 package hudson.plugins.clearcase.action;
 
-import java.util.HashSet;
-
 import hudson.Launcher;
 import hudson.plugins.clearcase.AbstractWorkspaceTest;
 import hudson.plugins.clearcase.ClearTool;
@@ -14,158 +12,191 @@ import org.junit.Test;
 
 public class UcmSnapshotCheckoutActionTest extends AbstractWorkspaceTest {
 
-	private Mockery context;
+    private Mockery context;
 
-	private ClearTool clearTool;
-	private Launcher launcher;
+    private ClearTool clearTool;
 
-	@Before
-	public void setUp() throws Exception {
-		createWorkspace();
-		context = new Mockery();
-		clearTool = context.mock(ClearTool.class);
-	}
+    private Launcher launcher;
 
-	@After
-	public void teardown() throws Exception {
-		deleteWorkspace();
-	}
+    @Before
+    public void setUp() throws Exception {
+        createWorkspace();
+        context = new Mockery();
+        clearTool = context.mock(ClearTool.class);
+    }
 
-	@Test
-	public void testFirstTime() throws Exception {
-		context.checking(new Expectations() {
-			{
-				one(clearTool).mkview("viewname", "stream");
-				one(clearTool).update("viewname", "loadrule");
-			}
-		});
+    @After
+    public void teardown() throws Exception {
+        deleteWorkspace();
+    }
 
-		CheckOutAction action = new UcmSnapshotCheckoutAction(clearTool,
-				"stream", "loadrule");
-		action.checkout(launcher, workspace, "viewname");
+    @Test
+    public void testFirstTime() throws Exception {
+        context.checking(new Expectations() {
+            {
+                one(clearTool).mkview("viewname", "stream");
+                one(clearTool).update("viewname", "loadrule");
+            }
+        });
 
-		context.assertIsSatisfied();
-	}
+        CheckOutAction action = new UcmSnapshotCheckoutAction(clearTool,
+                "stream", "loadrule");
+        action.checkout(launcher, workspace, "viewname");
 
-	@Test
-	public void testSecondTime() throws Exception {
-		workspace.child("viewname").mkdirs();
+        context.assertIsSatisfied();
+    }
 
-		context.checking(new Expectations() {
-			{
-				one(clearTool).catcs("viewname");
-				one(clearTool).rmview("viewname");
-				one(clearTool).mkview("viewname", "stream");
-				one(clearTool).update("viewname", "loadrule");
-			}
-		});
+    @Test
+    public void testSecondTime() throws Exception {
+        workspace.child("viewname").mkdirs();
 
-		CheckOutAction action = new UcmSnapshotCheckoutAction(clearTool,
-				"stream", "loadrule");
-		action.checkout(launcher, workspace, "viewname");
+        context.checking(new Expectations() {
+            {
+                one(clearTool).catcs("viewname");
+                one(clearTool).rmview("viewname");
+                one(clearTool).mkview("viewname", "stream");
+                one(clearTool).update("viewname", "loadrule");
+            }
+        });
 
-		context.assertIsSatisfied();
-	}
+        CheckOutAction action = new UcmSnapshotCheckoutAction(clearTool,
+                "stream", "loadrule");
+        action.checkout(launcher, workspace, "viewname");
 
-	@Test
-	public void testSecondTimeWithLoadRulesSatisfied() throws Exception {
-		final String viewName = "viewname";
-		final String loadRules = "abc/";
-		workspace.child(viewName).mkdirs();
+        context.assertIsSatisfied();
+    }
 
-		context.checking(new Expectations() {
-			{
-				one(clearTool).catcs(viewName);
-				will(returnValue("load " + loadRules));
-				one(clearTool).update(viewName, loadRules);
-			}
-		});
+    @Test
+    public void testSecondTimeWithLoadRulesSatisfied() throws Exception {
+        final String viewName = "viewname";
+        final String loadRules = "abc/";
+        workspace.child(viewName).mkdirs();
 
-		CheckOutAction action = new UcmSnapshotCheckoutAction(clearTool,
-				"stream", loadRules);
-		action.checkout(launcher, workspace, viewName);
+        context.checking(new Expectations() {
+            {
+                one(clearTool).catcs(viewName);
+                will(returnValue("load " + loadRules));
+                one(clearTool).update(viewName, loadRules);
+            }
+        });
 
-		context.assertIsSatisfied();
-	}
+        CheckOutAction action = new UcmSnapshotCheckoutAction(clearTool,
+                "stream", loadRules);
+        action.checkout(launcher, workspace, viewName);
 
-	@Test
-	public void testSecondTimeWithMultipleLoadRulesSatisfied() throws Exception {
-		final String viewName = "viewname";
+        context.assertIsSatisfied();
+    }
 
-		workspace.child(viewName).mkdirs();
+    @Test
+    public void testSecondTimeWithMultipleLoadRulesSatisfied() throws Exception {
+        final String viewName = "viewname";
 
-		context.checking(new Expectations() {
-			{
-				one(clearTool).catcs(viewName);
-				will(returnValue("load abc/\nload abcd"));
-				one(clearTool).update(viewName, "abc/");
-				one(clearTool).update(viewName, "abcd");
-			}
-		});
+        workspace.child(viewName).mkdirs();
 
-		CheckOutAction action = new UcmSnapshotCheckoutAction(clearTool,
-				"stream", "abc/\nabcd");
-		action.checkout(launcher, workspace, viewName);
+        context.checking(new Expectations() {
+            {
+                one(clearTool).catcs(viewName);
+                will(returnValue("load abc/\nload abcd"));
+                one(clearTool).update(viewName, "abc/");
+                one(clearTool).update(viewName, "abcd");
+            }
+        });
 
-		context.assertIsSatisfied();
-	}
+        CheckOutAction action = new UcmSnapshotCheckoutAction(clearTool,
+                "stream", "abc/\nabcd");
+        action.checkout(launcher, workspace, viewName);
 
-	@Test
-	public void testSecondTimeWithMultipleLoadRulesNotSatisfied()
-			throws Exception {
-		final String viewName = "viewname";
-		workspace.child(viewName).mkdirs();
+        context.assertIsSatisfied();
+    }
 
-		context.checking(new Expectations() {
-			{
-				one(clearTool).catcs(viewName);
-				will(returnValue("load abc/"));
-				one(clearTool).rmview(viewName);
-				one(clearTool).mkview(viewName, "stream");
-				one(clearTool).update(viewName, "abc/");
-				one(clearTool).update(viewName, "abcd");
-			}
-		});
+    @Test
+    public void testSecondTimeWithMultipleLoadRulesNotSatisfied()
+            throws Exception {
+        final String viewName = "viewname";
+        workspace.child(viewName).mkdirs();
 
-		CheckOutAction action = new UcmSnapshotCheckoutAction(clearTool,
-				"stream", "abc/\nabcd");
-		action.checkout(launcher, workspace, viewName);
+        context.checking(new Expectations() {
+            {
+                one(clearTool).catcs(viewName);
+                will(returnValue("load abc/"));
+                one(clearTool).rmview(viewName);
+                one(clearTool).mkview(viewName, "stream");
+                one(clearTool).update(viewName, "abc/");
+                one(clearTool).update(viewName, "abcd");
+            }
+        });
 
-		context.assertIsSatisfied();
-	}
+        CheckOutAction action = new UcmSnapshotCheckoutAction(clearTool,
+                "stream", "abc/\nabcd");
+        action.checkout(launcher, workspace, viewName);
 
-	@Test
-	public void testMultipleLoadRules() throws Exception {
-		context.checking(new Expectations() {
-			{
-				one(clearTool).mkview("viewname", "stream");
-				one(clearTool).update("viewname", "loadrule");
-				one(clearTool).update("viewname", "another\t loadrule");
-			}
-		});
+        context.assertIsSatisfied();
+    }
 
-		CheckOutAction action = new UcmSnapshotCheckoutAction(clearTool,
-				"stream", "loadrule\r\nanother\t loadrule");
-		action.checkout(launcher, workspace, "viewname");
+    @Test
+    public void testMultipleLoadRules() throws Exception {
+        context.checking(new Expectations() {
+            {
+                one(clearTool).mkview("viewname", "stream");
+                one(clearTool).update("viewname", "loadrule");
+                one(clearTool).update("viewname", "another\t loadrule");
+            }
+        });
 
-		context.assertIsSatisfied();
-	}
+        CheckOutAction action = new UcmSnapshotCheckoutAction(clearTool,
+                "stream", "loadrule\r\nanother\t loadrule");
+        action.checkout(launcher, workspace, "viewname");
 
-	@Test
-	public void testMultipleWindowsLoadRules() throws Exception {
-		context.checking(new Expectations() {
-			{
-				one(clearTool).mkview("viewname", "stream");
-				one(clearTool).update("viewname", "\\ \\Windows");
-				one(clearTool).update("viewname", "\\\\C:\\System32");
-			}
-		});
+        context.assertIsSatisfied();
+    }
 
-		CheckOutAction action = new UcmSnapshotCheckoutAction(clearTool,
-				"stream", "\\ \\Windows\n\\\\C:\\System32");
-		action.checkout(launcher, workspace, "viewname");
+    @Test
+    public void testSecondTimeWithRealMultipleLoadRulesSatisfied()
+            throws Exception {
+        final String viewName = "viewname";
 
-		context.assertIsSatisfied();
-	}
+        final String catcsOutput = "ucm\nidentity UCM.Stream oid:b689a462.74e011dd.8df7.00:16:35:7e:e1:93@vobuuid:9851d17f.c5aa11dc.9e7d.00:16:35:7e:e1:93 2\n"
+                + "# ONLY EDIT THIS CONFIG SPEC IN THE INDICATED \"CUSTOM\" AREAS\n#\n# This config spec was automatically generated by the UCM stream\n"
+                + "# \"base_1_4_Integration\" at 2008-09-02T16:12:44+02.\n#\n\n\n\n# Select checked out versions\nelement * CHECKEDOUT\n# Component selection rules..."
+                + "element \"[7781d0a2c5aa11dc9e670016357ee193=\base]/...\" .../base_1_4_Integration/LATEST\n"
+                + "element \"[7781d0a2c5aa11dc9e670016357ee193=\base]/...\" base_Main_080902_bjsa01_dsme_deliver -mkbranch base_1_4_Integration\n"
+                + "element \"[7781d0a2c5aa11dc9e670016357ee193=\base]/...\" /main/0 -mkbranch base_1_4_Integration\n"
+                + "\n\n\nend ucm\n\n#UCMCustomElemBegin - DO NOT REMOVE - ADD CUSTOM ELEMENT RULES AFTER THIS LINE\n#UCMCustomElemEnd - DO NOT REMOVE - END CUSTOM ELEMENT RULES"
+                + "\n# Non-included component backstop rule: no checkouts\nelement * /main/0 -ucm -nocheckout\n\n#UCMCustomLoadBegin - DO NOT REMOVE - ADD CUSTOM LOAD RULES AFTER THIS LINE\n"
+                + "load /vobs/base";
+
+        workspace.child(viewName).mkdirs();
+
+        context.checking(new Expectations() {
+            {
+                one(clearTool).catcs(viewName);
+                will(returnValue(catcsOutput));
+                one(clearTool).update(viewName, "vobs/base");
+            }
+        });
+
+        CheckOutAction action = new UcmSnapshotCheckoutAction(clearTool,
+                "stream", "vobs/base");
+        action.checkout(launcher, workspace, viewName);
+
+        context.assertIsSatisfied();
+    }
+
+    @Test
+    public void testMultipleWindowsLoadRules() throws Exception {
+        context.checking(new Expectations() {
+            {
+                one(clearTool).mkview("viewname", "stream");
+                one(clearTool).update("viewname", "\\ \\Windows");
+                one(clearTool).update("viewname", "\\\\C:\\System32");
+            }
+        });
+
+        CheckOutAction action = new UcmSnapshotCheckoutAction(clearTool,
+                "stream", "\\ \\Windows\n\\\\C:\\System32");
+        action.checkout(launcher, workspace, "viewname");
+
+        context.assertIsSatisfied();
+    }
 
 }
