0c6f31d2dd99bd701e77967b750f742c7226c7c5
lynggaard <lynggaard@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-10-19 15:22:04 +0000

    clearcase-plugin: fake cleartool command that reads hudson+clearcase logfiles
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/plugins@12651 71c3de6d-444a-0410-be80-ed276b4c234a

---
+++ src/test/java/hudson/plugins/clearcase/fakecleartool/ClearTool.java
@@ -0,0 +1,90 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hudson.plugins.clearcase.fakecleartool;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author henrik
+ */
+public class ClearTool {
+
+    static List<String> lines = new ArrayList<String>();
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Error: too few aagruments");
+            System.exit(0);
+        }
+        readLogfile(args[0]);
+
+        //System.out.println("Command:" +args[1]);
+        //System.out.println("linex:" +lines.size());
+        boolean started = false;
+        for (String line : lines) {
+
+            if (!started && line.contains("cleartool") && line.contains(args[1])) {
+                //System.out.println("found possible start:"+line);
+                if (args[1].equals("lsactivity") && line.contains(args[args.length-1])) {
+                    started = true;
+                    continue;
+                }
+                if (args[1].equals("lshistory")) {
+                    //System.out.println("statred:");
+                    started = true;
+                    continue;
+                }
+            }
+
+            if (started && line.contains("cleartool")) {
+                //System.out.println("found end:"+line);
+                break;
+            }
+
+            if (started) {
+                System.out.println(line);
+            }
+        }
+
+
+
+    }
+
+    private static void readLogfile(String filename) {
+        try {
+            FileReader fr = new FileReader(filename);
+            BufferedReader br = new BufferedReader(fr);
+
+            boolean done = false;
+            while (!done) {
+                String line = br.readLine();
+                if (line != null) {
+                    lines.add(line);
+                } else {
+                    done = true;
+                }
+            }
+            br.close();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+            Logger.getLogger(ClearTool.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            Logger.getLogger(ClearTool.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+}
