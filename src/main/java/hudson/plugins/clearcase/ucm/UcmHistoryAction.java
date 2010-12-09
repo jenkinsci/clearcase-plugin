273af071eb14061a09aabd36d8d129930118f2c8
vlatombe <vlatombe@NCEVLATOMBE.nce.amadeus.net>
2010-12-09 16:56:31 +0100

    Remove diffbl

--- src/main/java/hudson/plugins/clearcase/ucm/UcmHistoryAction.java
+++ src/main/java/hudson/plugins/clearcase/ucm/UcmHistoryAction.java
@@ -159,6 +159,51 @@ public class UcmHistoryAction extends AbstractHistoryAction {
     }
 
     @Override
+	public boolean hasChanges(Date sinceTime, String viewPath, String viewTag,
+			String[] branchNames, String[] viewPaths) throws IOException,
+			InterruptedException {
+    	return hasChangesOnCurrentBranch(sinceTime, viewPath, viewTag, branchNames, viewPaths) || haveFoundationBaselinesChanged();
+	}
+
+	private boolean haveFoundationBaselinesChanged() {
+		if (oldBaseline == null) {
+			return newBaseline != null;
+		}
+		List<Baseline> oldBaselines = oldBaseline.getBaselines();
+        List<Baseline> newBaselines = newBaseline.getBaselines();
+        if (ObjectUtils.equals(oldBaselines, newBaselines)) {
+            return false;
+        }
+        for (final Baseline oldBl : oldBaselines) {
+            String bl1 = oldBl.getBaselineName();
+            final String comp1 = oldBl.getComponentName();
+            // Lookup the component in the set of new baselines
+            Baseline newBl = (Baseline) CollectionUtils.find(newBaselines, new Predicate() {
+                @Override
+                public boolean evaluate(Object bl) {
+                    return StringUtils.equals(comp1, ((Baseline) bl).getComponentName());
+                }
+            });
+            // If we cannot find a new baseline, log and skip
+            if (newBl == null) {
+                cleartool.getLauncher().getListener().getLogger().print("Old Baseline " + bl1 + " for component " + comp1 + " couldn't be found in the new set of baselines.");
+                continue;
+            }
+            String bl2 = newBl.getBaselineName();
+            if (!StringUtils.equals(bl1, bl2)) {
+                return true;
+            }
+        }
+        return false;
+	}
+
+	private boolean hasChangesOnCurrentBranch(Date sinceTime, String viewPath, String viewTag, String[] branchNames, String[] viewPaths) throws IOException, InterruptedException {
+		List<HistoryEntry> entries = super.runLsHistory(sinceTime, viewPath, viewTag, branchNames, viewPaths);
+        List<HistoryEntry> filtered = filterEntries(entries);
+        return filtered.size() > 0;
+	}
+
+	@Override
     protected List<HistoryEntry> runLsHistory(Date sinceTime, String viewPath, String viewTag, String[] branchNames, String[] viewPaths) throws IOException,
             InterruptedException {
         List<HistoryEntry> history = super.runLsHistory(sinceTime, viewPath, viewTag, branchNames, viewPaths);
@@ -187,15 +232,16 @@ public class UcmHistoryAction extends AbstractHistoryAction {
             }
             String bl2 = newBl.getBaselineName();
             if (!StringUtils.equals(bl1, bl2)) {
-                List<String> versions = UcmCommon.getDiffBlVersions(cleartool, viewPath, "baseline:" + bl1, "baseline:" + bl2);
-                for (String version : versions) {
-                    try {
-                        parseLsHistory(new BufferedReader(cleartool.describe(getHistoryFormatHandler().getFormat() + COMMENT + LINEEND, version)), history);
-                    } catch (ParseException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-                }
+            	history.add(new HistoryEntry());
+//                List<String> versions = UcmCommon.getDiffBlVersions(cleartool, viewPath, "baseline:" + bl1, "baseline:" + bl2);
+//                for (String version : versions) {
+//                    try {
+//                        parseLsHistory(new BufferedReader(cleartool.describe(getHistoryFormatHandler().getFormat() + COMMENT + LINEEND, version)), history);
+//                    } catch (ParseException e) {
+//                        // TODO Auto-generated catch block
+//                        e.printStackTrace();
+//                    }
+//                }
             }
         }
         return history;
