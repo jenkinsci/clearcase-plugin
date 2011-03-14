8346a1600fb460418013ff87fa78ae591bc8c71f
Christophe Guillon <christophe.guillon@st.com>
2011-03-14 14:37:11 +0100

    Added detection of unexpected empty lsview output.
    When a lsview command returned an empty string, the plugin silently
    created duplicate snapshot views.
    Now when the ct lsview -cview -s returns an empty view an exception is raised.

--- src/main/java/hudson/plugins/clearcase/ClearToolExec.java
+++ src/main/java/hudson/plugins/clearcase/ClearToolExec.java
@@ -331,6 +331,9 @@ public abstract class ClearToolExec implements ClearTool {
                 throw exceptions.get(0);
             }
         }
+        if (StringUtils.isBlank(output)) {
+            throw new IOException("Unexpected output for command \"cleartool lsview -cview -s\":" + output);
+        }
         return output;
     }
 
