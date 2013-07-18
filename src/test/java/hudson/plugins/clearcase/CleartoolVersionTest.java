cc6098827c378960c81d8507ce5bee87b68258e2
Vincent Latombe <vincent@latombe.net>
2013-07-18 16:10:19 +0200

    Add some more tests and handle in a generic way old 2xxx versions.

--- src/test/java/hudson/plugins/clearcase/CleartoolVersionTest.java
+++ src/test/java/hudson/plugins/clearcase/CleartoolVersionTest.java
@@ -15,6 +15,7 @@ public class CleartoolVersionTest extends AbstractWorkspaceTest {
         parseFileThenAssertVersion("ct-version-1.log", "7.1.2.6");
         parseFileThenAssertVersion("ct-version-2.log", "7.1.1.1");
         parseFileThenAssertVersion("ct-version-4.log", "2003.06.10");
+        parseFileThenAssertVersion("ct-version-5.log", "2002.05.00");
     }
 
     @Test(expected = CleartoolVersionParsingException.class)
@@ -27,7 +28,12 @@ public class CleartoolVersionTest extends AbstractWorkspaceTest {
         CleartoolVersion v7 = new CleartoolVersion("7");
         CleartoolVersion v7111 = new CleartoolVersion("7.1.1.1");
         CleartoolVersion v7126 = new CleartoolVersion("7.1.2.6");
+        CleartoolVersion v2002 = new CleartoolVersion("2002.05.00");
         CleartoolVersion v2003 = new CleartoolVersion("2003.06.10");
+        assertTrue(v2002.compareTo(v2003) < 0);
+        assertTrue(v2003.compareTo(v2002) > 0);
+        assertTrue(v2002.compareTo(v7) < 0);
+        assertTrue(v7.compareTo(v2002) > 0);
         assertTrue(v2003.compareTo(v7) < 0);
         assertTrue(v2003.compareTo(v7111) < 0);
         assertTrue(v2003.compareTo(v7126) < 0);
